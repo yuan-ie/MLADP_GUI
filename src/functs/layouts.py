fb1bc92eb637351d8aefef86c2593d7ca1ed4383
yuan-ie <angela.nghiem1@gmail.com>
2024-11-21 00:25:47 -0500

    view page and about page finished

--- src/functs/layouts.py
+++ src/functs/layouts.py
@@ -1,15 +1,6 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QGroupBox, QListWidget
 from PyQt6.QtCore import Qt
 
-def title_block():
-    '''
-    Block one:
-        displays only the title of the window -- center-aligned.
-    '''
-    title_label = QLabel("My Application Title")
-    title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-    title_label.setStyleSheet("font-size: 24px; font-weight: bold;")
-    return title_label
 
 def menu_block():
     '''
@@ -25,9 +16,9 @@ def menu_block():
     button_layout.setSpacing(20)  # Space between buttons
     
     # Create buttons with rounded corners
-    button1 = QPushButton("About Me")
-    button2 = QPushButton("All Data")
-    button3 = QPushButton("Etc.")
+    button1 = QPushButton("About")
+    button2 = QPushButton("Viewer")
+    button3 = QPushButton("Data Types")
     
     # Add buttons to the layout
     button_layout.addWidget(button1)
@@ -37,4 +28,14 @@ def menu_block():
     # Add to layout
     block_two.addLayout(button_layout)
 
-    return block_two, button1, button2
+    return block_two, button1, button2, button3
+
+def title_block():
+    '''
+    Block one:
+        displays only the title of the window -- center-aligned.
+    '''
+    title_label = QLabel("Biopsy Slide Viewer")
+    title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    title_label.setStyleSheet("font-size: 24px;") # font-weight: bold;
+    return title_label
